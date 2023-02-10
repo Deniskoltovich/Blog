399871a4d22b66818a29e0ecda80d33cbc5108b6
Denis Koltovich <koltovich1123@gmail.com>
2023-02-10 13:33:33 +0300

    added post searching & indexing models & transactions

--- blogs/models.py
+++ blogs/models.py
@@ -5,8 +5,9 @@ from django.contrib.auth.models import User
 class Blog(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=64, null=False, blank=False)
-    description = models.CharField(max_length=128, null=True, blank=True)
+    description = models.CharField(max_length=256, null=True, blank=True)
     creation_date = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=True)
 
     class Meta:
         indexes = [
